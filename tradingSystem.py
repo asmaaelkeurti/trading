6ec9292a2d1a2aee231cf2bd4557b5ec6aa03a50
asmaaelkeurti <asmaaelkeurti@aliyun.com>
2018-06-07 17:21:34 +0800

    trend_following_done

--- tradingSystem.py
+++ tradingSystem.py
@@ -1,15 +1,76 @@
-import pandas as pd
+import numpy as np
 
-def trend_following_strategy(data):
-    low_price = data['price'].iloc[-1]
-    high_price = data['price'].iloc[-1]
-    current_price = data['price'].iloc[-1]
+class trading_strategy:
+    def __init__(self,initial_cash):
+        self.shares = 0
+        self.current_market_value = 0
+        self.cash = initial_cash
+        self.price = 0
+        
+        self.enter_price = 0
+        self.trailing_low = 0
+        self.trailing_high = 0
+        
+        self.history_price = np.array([])
     
-    current_price = row['price']
-    if current_price > high_price:
-        high_price = current_price
+    def balance(self):
+        return self.cash + self.current_market_value
+        
+    def update_price(self,price):
+        self.price = price
+        self.current_market_value = self.shares*price
+        
+    def adjust_positions_to(self,shares):
+        self.close_position()
+        self.shares = shares
+        self.cash = self.cash - self.shares*self.price
+        self.update_price(self.price)
+        
+    def close_position(self):
+        self.cash = self.balance()
+        self.current_market_value = 0
+        self.shares = 0
+    
+    def empty_history_price(self):
+        self.history_price = np.array([self.history_price[-1]])
+        
+    def __str__(self):
+        return """
+                shares = %s
+                current_market_value = %s
+                cash = %s
+                price = %s
+                balance = %s
+                """ % ( self.shares,
+                        self.current_market_value,
+                        self.cash,
+                        self.price,
+                        self.balance())
+
+
+class trend_following_strategy(trading_strategy):
+    def new_day(self,new_price,scale=0.03):
+        self.history_price = np.append(self.history_price,new_price)
+        self.update_price(new_price)
+        if new_price >= self.history_price.min()*(1+scale):
+            self.empty_history_price()
+            self.adjust_positions_to(int(self.balance()*0.8/new_price))
+        elif new_price <= self.history_price.max()*(1-scale):
+            self.empty_history_price()
+            self.adjust_positions_to(int(-self.balance()*0.8/new_price))
+        
+            
+
+class mean_reverse_strategy(trading_strategy):
+    pass
 
 
 def run(data):
     for index,row in data[::-1].iterrows():
         trend_following_strategy(row['price'])
+        
+        
+if __name__ == '__main__':
+    a = trend_following_strategy(10000)
+    
+    
